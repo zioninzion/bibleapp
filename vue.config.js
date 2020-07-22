f6e5dbf1dd74483e8f409358c28ebf743f3ef46e
Armad Ellis <zamirellis19@gmail.com>
2020-07-22 19:22:33 -0400

    deploy2

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 // vue.config.js
 module.exports = {
-publicPath: process.env.NODE_ENV!= "production" ? "/bibleapp/" : "/",
+publicPath: process.env.NODE_ENV === "production" ? "/bibleapp/" : "/",
 };
 
